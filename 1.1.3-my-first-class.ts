abec28205b86e44129191e8ce84801b94e155dc9
MahiroKamui <alexibenjamin98@gmail.com>
2026-08-17 13:26:25 +0300

    My first class!

---
+++ 1.1.3-my-first-class.ts
@@ -0,0 +1,12 @@
+class Book {
+    ISBN: number = 0;
+    title: string = "";
+    author: string = "";
+    pageCount: number = 0;
+}
+
+
+const bookOne = new Book()
+bookOne.ISBN = 9780007458424
+bookOne.author = "J.R.R. Tolkien"
+bookOne
