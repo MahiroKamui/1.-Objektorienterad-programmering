94c2afe4372c925398d8dc9de9612a3525640872
MahiroKamui <alexibenjamin98@gmail.com>
2026-08-17 13:27:07 +0300

    my first commit!

--- 1.1.3-my-first-class.ts
+++ 1.1.3-my-first-class.ts
@@ -8,5 +8,16 @@ class Book {
 
 const bookOne = new Book()
 bookOne.ISBN = 9780007458424
+bookOne.title = "The Hobbit"
 bookOne.author = "J.R.R. Tolkien"
-bookOne
+bookOne.pageCount = 310
+
+
+const bookTwo = new Book()
+bookTwo.ISBN = 9789129723946
+bookTwo.title = "Harry Potter and the philosopher's stone"
+bookTwo.author = "J.K. Rowling"
+bookTwo.pageCount = 384
+
+console.log(bookOne)
+console.log(bookTwo)
