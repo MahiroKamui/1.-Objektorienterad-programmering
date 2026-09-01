24a9ebec0a9bc8f46fba1b64657679be70501974
MahiroKamui <alexibenjamin98@gmail.com>
2026-09-01 10:13:25 +0300

    1.3.1 finished

---
+++ 1.3.1-public-private/bank-account.ts
@@ -0,0 +1,36 @@
+
+
+export class BankAccount {
+    public accountHolder: string;
+    private balance: number;
+
+
+    deposit(amount: number) {
+        if (amount <= 0) {
+            console.log("You can not deposit 0 or less, please enter a value larger than 0")
+            return
+        }
+        this.balance += amount;
+    }
+    withdraw(amount: number) {
+        const currentBalance = this.balance;
+        if (currentBalance-amount < 0 || amount == 0) {
+            console.log(`You entered an invalid amount: ${amount}`)
+            console.log("Please check that the requested amount is larger than 0 and doesnt exceed your current balance")
+            console.log(`current balance: ${this.balance}`)
+            return
+        }
+        this.balance -= amount;
+    }
+    showInfo() {
+        console.log(`account holder: ${this.accountHolder}`);
+        console.log(`current balance: ${this.balance}`);
+    }
+    getBalance() {
+        return this.balance;
+    }
+    constructor(name: string, balance: number) {
+        this.accountHolder = name;
+        this.balance = balance;
+    }
+}
