e7a64427101f911f1b8e61a2bbe83395e35e3b19
MahiroKamui <alexibenjamin98@gmail.com>
2026-09-22 13:35:38 +0300

    started 1.3.4

---
+++ 1.3.4-interactive-bank-program/bank-account.ts
@@ -0,0 +1,17 @@
+
+
+export class BankAccount {
+    private _accountOwner: string
+    private _balance: number
+
+    get accountOwner(): string {
+        return this._accountOwner
+    }
+    get balance(): number {
+        return this._balance
+    }
+
+    constructor(accountOwner: string, balance: number) {
+        if()
+    }
+}
